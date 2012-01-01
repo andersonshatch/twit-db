3491903ffad5b0ef501cca13c888cafca511595c
Josh Anderson <andersonshatch@gmail.com>
2012-01-01 16:22:43 +0000

    Clean up index.php and add mustache tweet template

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<?php $time_start = microtime(true);
-ob_implicit_flush(1);
+<?php 
 if(is_readable('config.php')){
 	require_once 'config.php';
 }else{
@@ -20,6 +19,7 @@ if(is_readable('config.php')){
 <script type="text/javascript" src="bootstrap/js/bootstrap-twipsy.js"></script>
 <script type="text/javascript" src="js/main.js"></script>
 <script type="text/javascript" src="js/timeago.js"></script>
+<script type="text/javascript" src="js/jquery.mustache.js"></script>
 <script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>
 </head>
 <body>
@@ -28,141 +28,81 @@ if(is_readable('config.php')){
 	<div class="fill">
 		<div class="container">
 			<h3><a href="#" style="margin-left: -22px;">Twit-DB</a></h3>
-			<form id="search-form" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
-				<input id="search-text" class="" name="text" value="<?php if(array_key_exists("text", $_POST)) echo htmlentities($_POST['text']); ?>" placeholder="Text" />
-				( <input id="search-username" class=""  name="username" value="<?php if(array_key_exists("username", $_POST)) echo htmlentities($_POST['username']); ?>" placeholder="Username <?php if( defined("MENTIONS_TIMELINE") && MENTIONS_TIMELINE == "true") echo "(@me for mentions)";?>" />
-				Retweets <input id="search-retweets" class="" name="retweets" type="checkbox" <?php if(array_key_exists("retweets", $_POST) && $_POST['retweets'] = 'on') echo 'checked="checked"'; ?> /> )
-				<input id="search-limit" class="" name="limit" value="<?php if(array_key_exists("limit", $_POST)) echo htmlentities($_POST['limit']); ?>" placeholder="Limit" />
+			<form id="search-form" action="" method="POST">
+				<input id="search-text" class="" name="text" value="" placeholder="Text" />
+				( <input id="search-username" class=""  name="username" value="" placeholder="Username <?php if( defined("MENTIONS_TIMELINE") && MENTIONS_TIMELINE == "true") echo "(@me for mentions)";?>" />
+				Retweets <input id="search-retweets" class="" name="retweets" type="checkbox" /> )
+				<input id="search-limit" class="" name="limit" value="" placeholder="Limit" />
 				<input type="submit" value="Submit" />
 			</form>
 		</div><!-- container -->
 	</div><!-- fill -->
 </div><!-- topbar -->
-
-<?php
-
-include 'lib/Timesince.php';
-require_once 'lib/linkify_tweet.php';
-require_once 'lib/buildQuery.php';
-
-$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
-$mysqli->set_charset("utf8");
-$GLOBALS['queryCount'] = 0;
-$queryString = buildQuery($_POST, $mysqli);
-$query = $mysqli->query($queryString);
-$GLOBALS['queryCount']++;
-?>
 <div id="container" class="container">
-<div class="content">
-<div class="stream" style="padding-top: 60px;">
-<?php
-$rowCount = $mysqli->affected_rows;
-echo <<<HTML
-	<div class="page-header">
-		$rowCount matching tweets<br />
-	</div>
-HTML;
-
-$favoriteURLBase = "https://twitter.com/intent/favorite/?tweet_id=";
-$retweetURLBase = "https://twitter.com/intent/retweet/?tweet_id=";
-$replyURLBase = "https://twitter.com/intent/tweet/?in_reply_to=";
-while ($row = $query->fetch_array(MYSQLI_ASSOC)){
-
-    $favouriteURL = "$favoriteURLBase{$row['id']}";
-    $retweetURL = "$retweetURLBase{$row['id']}";
-    $replyURL = "$replyURLBase{$row['id']}";
-    
-    $text = linkify_tweet($row['text'], $row['entities_json']);
-    if($row['retweeted_by_user_id'] != null){
-	$tweetMeta = <<<HTML
-	    \n<span class="retweet-icon"></span>by <a class="tweet-screen-name user-profile-link" href="https://twitter.com/{$row['retweeted_by_screen_name']}">{$row['retweeted_by_screen_name']}</a>
-HTML;
-    }else
-	$tweetMeta = "";
-
-    if($row['place_full_name'] != null){
-	$extraIcons = <<<HTML
-	    	\n<div class="extra-icons">
-	    		<span class="geo-pin"></span>
-		</div>
-HTML;
-	$placeName = <<<HTML
-		from {$row['place_full_name']}
-HTML;
-    }else{
-	$extraIcons = "";
-	$placeName = "";
-    }
-$createdat = date_create_from_format('Y-m-d H:i:s', $row['created_at']);
-$timestamp = Timesince($createdat->format('U'));
-echo <<<HTML
-	<div class="row tweet" data-item-id="{$row['id']}">
-		<div class="row span16 columns">
-			<div class="span1 columns tweet-image">
-				<br />
-				<a class="user-profile-link" href="https://twitter.com/{$row['screen_name']}">
-					<img width="48" height="48" class="user-profile-link" alt="{$row['screen_name']}'s avatar" original={$row['profile_image_url']}>
-				</a>
-			</div>
-			<div class="span14 columns tweet-content">
-				<div class="tweet-names">
-					<h3><a class="tweet-screen-name user-profile-link" href="https://twitter.com/{$row['screen_name']}">{$row['screen_name']}</a>
-					<small class="tweet-full-name">{$row['name']}$tweetMeta</small>
-					</h3>
-				</div>
-				<div class="tweet-text">
-					$text
-				</div>
-			
-				<blockquote class="tweet-metadata">
-					<small>
-						<a href="https://twitter.com/#!/{$row['screen_name']}/status/{$row['id']}" class="tweet-timestamp" target="_blank" title="{$createdat->format('G:i M jS \'y')}" data-timestamp="{$createdat->format('c')}">
-							$timestamp ago 
-						</a>
-						<span class="tweet-source">
-							via {$row['source']} $placeName 
-						</span>
-						<span class="tweet-actions" style="visibility: hidden;">
-							<a href="{$favouriteURL}" class="favorite-action" title="Favorite">
-								<i></i>Favorite
-							</a>
-						<a href="$retweetURL" class="retweet-action" title="Retweet">
-							<i></i>Retweet
-						</a>
-						<a href="$replyURL" class="reply-action" title="Reply">
-							<i></i>Reply
-						</a>
-						</span>
-					</small>
-				</blockquote>
-			</div>
-		</div>
-	</div>
-
-HTML;
-
-
-
-}
-
-$mysqli->close();
-?>
+<div class="content" style="padding-top: 60px;">
+<div class="page-header">
+	<span id="tweet-count"></span>
+</div>
+<div id="stream">
+</div>
 <script type="text/javascript">
 twttr.anywhere(function (T) {
 	T(".username, .tweet-screen-name, .icons > em > a").hovercards({ linkify: false, expanded: true });
 });
 </script>
 </div><!--content-->
-<div class="page-header"></div>
-<div class="gen-stats">
-<?php $time_end = microtime(true);
-$time = $time_end - $time_start;
-echo "Created in ".substr($time, 0, 6)." seconds.<br />Used {$GLOBALS['queryCount']} ";
-$queries = ($GLOBALS['queryCount'] == 1) ? "query." : "queries.";
-echo $queries;
-?>
-</div><!--gen-stats-->
-</div><!--content-->
 </div><!--container-->
+<script type="text/mustache-template" id="tweet-template">
+{{#tweets}}
+<div class="row tweet" data-item-id="{{id}}">
+	<div class="row span16 columns">
+		<div class="span1 columns tweet-image">
+			<br />
+			<a class="user-profile-link" href="https://twitter.com/{{screen_name}}">
+				<img width="48" height="48" class="user-profile-link" alt="{{screen_name}}'s avatar" src="{{profile_image_url}}">
+			</a>
+		</div>
+		<div class="span14 columns tweet-content">
+			<div class="tweet-names">
+				<h3>
+					<a class="tweet-screen-name user-profile-link" href="https://twitter.com/{{screen_name}}">{{screen_name}}</a>
+					<small class="tweet-full-name">
+						{{name}}
+						{{#retweeted_by_user_id}}
+							<span class="retweet-icon"></span>by <a class="tweet-screen-name user-profile-link" href="https://twitter.com/{{retweeted_by_screen_name}}">{{retweeted_by_screen_name}}</a>
+						{{/retweeted_by_user_id}}
+					</small>
+				</h3>
+			</div>
+			<div class="tweet-text">
+				{{{text}}}
+			</div>
+
+			<blockquote class="tweet-metadata">
+				<small>
+					<a href="https://twitter.com/{{screen_name}}/status/{{id}}" class="tweet-timestamp" target="_blank" title="{{timestamp_title}}" data-timestamp="{{datetime}}">
+						{{created_at}}
+					</a>
+					<span class="tweet-source">
+						via {{{source}}}{{#place_full_name}} from {{place_full_name}}{{/place_full_name}}
+					</span>
+					<span class="tweet-actions" style="visibility: hidden;">
+						<a href="https://twitter.com/intent/favorite/?tweet_id={{id}}" class="favorite-action" title="Favorite">
+							<i></i>Favorite
+						</a>
+						<a href="https://twitter.com/intent/retweet/?tweet_id={{id}}" class="retweet-action" title="Retweet">
+							<i></i>Retweet
+						</a>
+						<a href="https://twitter.com/intent/tweet/?in_reply_to={{id}}" class="reply-action" title="Reply">
+							<i></i>Reply
+						</a>
+					</span>
+				</small>
+			</blockquote>
+		</div>
+	</div>
+</div>
+{{/tweets}}
+</script>
 </body>
 </html>
