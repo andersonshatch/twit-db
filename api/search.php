716fb727e353e3eaab2e31c6f26f188e0e664d87
Josh Anderson <andersonshatch@gmail.com>
2015-09-15 22:11:37 +0100

    Update search API with query builder changes

--- api/search.php
+++ api/search.php
@@ -6,7 +6,8 @@ require_once("../lib/ConfigHelper.php");
 ConfigHelper::requireConfig("../config.php");
 $mysqli = ConfigHelper::getDatabaseConnection();
 
-require_once('../lib/buildQuery.php');
+require_once('../lib/QueryHolder.php');
+require_once('../lib/SearchQueryBuilder.php');
 require_once('../lib/TweetFormatter.php');
 
 header('Content-Type: application/json; charset=utf-8');
@@ -17,13 +18,20 @@ $results = [];
 $sortAscending = array_key_exists('since_id', $_GET) && !array_key_exists('max_id', $_GET);
 
 if(array_key_exists("count-only", $_GET)) {
-	$rowCountQuery = $mysqli->query($countQuery = buildQuery($_GET, $mysqli, true, $sortAscending));
-	$rowCountRow = $rowCountQuery->fetch_row();
+	list($queryString, $queryParams) = SearchQueryBuilder::buildQuery($_GET, true, $sortAscending);
+	$rowCountQuery = QueryHolder::prepareAndHoldQuery($mysqli, $queryString);
+	SearchQueryBuilder::prepareQuery($rowCountQuery, $queryParams);
+	$rowCountQuery->execute();
+	$rowCountRow = $rowCountQuery->get_result()->fetch_array();
 	$results["matchingTweets"] = number_format($rowCountRow[0]);
-	$results["countQuery"] = $countQuery;
+	$results["countQuery"] = $queryString;
+	$results["queryParams"] = $queryParams;
 } else {
-	$query = $mysqli->query($queryString = buildQuery($_GET, $mysqli, false, $sortAscending));
-	$tweets = $query->fetch_all(MYSQLI_ASSOC);
+	list($queryString, $queryParams) = SearchQueryBuilder::buildQuery($_GET, false, $sortAscending);
+	$query = QueryHolder::prepareAndHoldQuery($mysqli, $queryString);
+	SearchQueryBuilder::prepareQuery($query, $queryParams);
+	$query->execute();
+	$tweets = $query->get_result()->fetch_all(MYSQLI_ASSOC);
 
 	if($sortAscending) {
 		//reverse so tweets are always served newest to oldest
@@ -57,6 +65,7 @@ if(array_key_exists("count-only", $_GET)) {
 
 	$results["tweets"] = $tweets;
 	$results["queryString"] = $queryString;
+	$results["queryParams"] = $queryParams;
 	$results["nextPage"] = $nextPage;
 	$results["previousPage"] = $previousPage;
 }
