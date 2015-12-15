e288b791b2fc877a58f9ad9bf1dc7275861a019b
Josh Anderson <andersonshatch@gmail.com>
2015-12-15 23:10:25 +0000

    Use model objects in tweet formatter

--- lib/TweetFormatter.php
+++ lib/TweetFormatter.php
@@ -2,42 +2,52 @@
 
 chdir(dirname(__FILE__));
 require_once('linkify_tweet.php');
+require_once('model/Tweet.php');
 
 class TweetFormatter {
 	private static $localDateTimeZone;
 	private static $utcDateTimeZone;
 
-	public static function formatTweets(&$tweets, $linkify = true) {
-		foreach($tweets as &$tweet) {
-			$tweet = TweetFormatter::formatTweet($tweet, $linkify);
+	public static function formatTweets(array $tweets, $linkify = true) {
+		$formattedTweets = [];
+		foreach($tweets as $tweetDictionary) {
+			$tweet = TweetFormatter::formatTweet($tweetDictionary, $linkify);
+			$formattedTweets[] = $tweet;
 		}
 
-		return $tweets;
+		return $formattedTweets;
 	}
 
-	public static function formatTweet(&$tweet, $linkify = true) {
+	public static function formatTweet(array $tweetDictionary, $linkify = true) {
 		if(self::$localDateTimeZone == null || self::$utcDateTimeZone == null) {
 			self::$localDateTimeZone = new DateTimeZone(date_default_timezone_get());
 			self::$utcDateTimeZone = new DateTimeZone('utc');
 		}
 
-		$tweet['id'] = (string) $tweet['id'];
-		$tweet['user_id'] = (string) $tweet['user_id'];
-		if($tweet['retweeted_by_user_id'] !== null) {
-			$tweet['retweeted_by_user_id'] = (string) $tweet['retweeted_by_user_id'];
+		$tweetDictionary['id'] = (string) $tweetDictionary['id'];
+		$tweetDictionary['user_id'] = (string) $tweetDictionary['user_id'];
+		if($tweetDictionary['retweeted_by_user_id'] !== null) {
+			$tweetDictionary['retweeted_by_user_id'] = (string) $tweetDictionary['retweeted_by_user_id'];
 		}
 
-		$createdAt = DateTime::createFromFormat('Y-m-d H:i:s', $tweet['created_at'], self::$utcDateTimeZone);
+		$createdAt = DateTime::createFromFormat('Y-m-d H:i:s', $tweetDictionary['created_at'], self::$utcDateTimeZone);
 		$createdAt->setTimezone(self::$localDateTimeZone);
-		$tweet['datetime'] = $createdAt->format('c');
-		$tweet['timestamp_title'] = $createdAt->format('G:i M jS \'y');
+		$dateTime = $createdAt->format('c');
+		$timestampTitle = $createdAt->format('G:i M jS \'y');
+
+		$entities = json_decode($tweetDictionary['entities_json']);
+		unset($tweetDictionary['entities_json']);
+
+		$tweet = new \TwitDB\Model\Tweet($tweetDictionary);
+
 		if($linkify) {
-			$tweet['text'] = linkify_tweet($tweet['text'], $tweet['entities_json']);
+			$tweet->setText(linkify_tweet($tweetDictionary['text'], $entities));
 		} else {
-			$tweet['entities'] = json_decode($tweet['entities_json']);
+			$tweet->setEntities($entities);
 		}
 
-		unset($tweet['entities_json']);
+		$tweet->setDateTime($dateTime);
+		$tweet->setTimestampTitle($timestampTitle);
 
 		return $tweet;
 	}
