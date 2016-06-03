8f7607d3b24234264452fe1dea87779b3fdce74c
Josh Anderson <andersonshatch@gmail.com>
2016-06-03 22:53:55 +0100

    Add a columnExists method

--- lib/DatabaseUtils.php
+++ lib/DatabaseUtils.php
@@ -1,6 +1,24 @@
 <?php
 
 class DatabaseUtils {
+	/**
+	 * See if a column exists in a table in this database
+	 * @param string $tableName name of the table to check
+	 * @param string $columnName name of the column to check for
+	 * @param mysqli $mysqli database handle
+	 * @return true if a column with $columnName exists on table $tableName in this database
+	 */
+	public static function columnExists($tableName, $columnName, mysqli $mysqli) {
+		$columnExistsSQL = "SELECT 1 FROM information_schema.columns
+							WHERE table_schema = '".DB_NAME."'
+							AND table_name = '".$tableName."'
+							AND column_name = '".$columnName."'";
+		if(!$mysqli->query($columnExistsSQL)->fetch_all()) {
+			return false;
+		}
+
+		return true;
+	}
 
 	/**
 	 * See if a table exists in this database
